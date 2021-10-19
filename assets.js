ca6f1ba9c258a61ae841fbebf02f9bab8d5c53b8
Dragoș Străinu <str.dr4605@gmail.com>
2021-10-19 19:35:41 +0300

    feat: replace 3D with images

---
+++ assets.js
@@ -0,0 +1,189 @@
+export const characters = [
+  "red",
+  "green",
+  "blue",
+  "white",
+  "black",
+  "yellow",
+  "purple",
+  "darkorange",
+  "magenta",
+];
+
+export const heroes = [
+  // {
+  //   scale: "1 1 1",
+  //   rotation: "-65 45 -45",
+  //   gltfModel: "heroes/bruce_lee/scene.gltf",
+  // },
+  // {
+  //   scale: "0.7 0.7 0.7",
+  //   rotation: "-90 -105 110",
+  //   gltfModel: "heroes/crocodile_gena/scene.gltf",
+  // },
+  // {
+  //   scale: "0.7 0.7 0.7",
+  //   rotation: "-80 35 -45",
+  //   gltfModel: "heroes/elon_musk/scene.gltf",
+  // },
+  // {
+  //   scale: "2 2 2",
+  //   rotation: "-90 5 -15",
+  //   gltfModel: "heroes/elvis_presley/scene.gltf",
+  // },
+  // {
+  //   scale: "1 1 1",
+  //   rotation: "-80 35 -45",
+  //   gltfModel: "heroes/garfield/scene.gltf",
+  // },
+  // {
+  //   scale: "2 2 2",
+  //   rotation: "-80 35 -45",
+  //   gltfModel: "heroes/gingerbread_man_shrek_2/scene.gltf",
+  // },
+  // {
+  //   scale: "2 2 2",
+  //   rotation: "-80 35 -45",
+  //   gltfModel: "heroes/jackie_chan/scene.gltf",
+  // },
+  // {
+  //   scale: "1.5 1.5 1.5",
+  //   rotation: "-90 55 -75",
+  //   gltfModel: "heroes/Masha/scene.gltf",
+  // },
+  // {
+  //   scale: "0.005 0.005 0.005",
+  //   rotation: "-90 55 -55",
+  //   gltfModel: "heroes/pacman_animated/scene.gltf",
+  // },
+  // {
+  //   scale: "1.2 1.2 1.2",
+  //   rotation: "-90 45 -55",
+  //   gltfModel: "heroes/pikachu/scene.gltf",
+  // },
+  // {
+  //   scale: "1.5 1.5 1.5",
+  //   rotation: "-110 55 -55",
+  //   gltfModel: "heroes/president_obama/scene.gltf",
+  // },
+  // {
+  //   scale: "0.05 0.05 0.05",
+  //   rotation: "-90 55 -75",
+  //   gltfModel: "heroes/sonic_the_hedgehog_running/scene.gltf",
+  // },
+  // {
+  //   scale: "0.5 0.5 0.5",
+  //   rotation: "-90 55 -75",
+  //   gltfModel: "heroes/spider-man_bust_statue/scene.gltf",
+  // },
+  // {
+  //   scale: "0.4 0.4 0.4",
+  //   rotation: "-100 55 -55",
+  //   gltfModel: "heroes/sponge_bob/scene.gltf",
+  // },
+  // {
+  //   scale: "0.09 0.09 0.09",
+  //   rotation: "-90 55 -55",
+  //   gltfModel: "heroes/voldemort_caricature/scene.gltf",
+  // },
+  // {
+  //   scale: "0.5 0.5 0.5",
+  //   rotation: "180 90 -90",
+  //   gltfModel: "heroes/wall-e/scene.gltf",
+  // },
+  // {
+  //   scale: "2.5 2.5 2.5",
+  //   rotation: "-90 40 -30",
+  //   gltfModel: "heroes/will_smith/scene.gltf",
+  // },
+  {
+    width: "3",
+    height: "3",
+    src: "heroes/mickey_mouse.png",
+  },
+  {
+    width: "3",
+    height: "3",
+    src: "heroes/santa_claus.png",
+  },
+  {
+    width: "3",
+    height: "3",
+    src: "heroes/bruce_lee.jpeg",
+  },
+  {
+    width: "3",
+    height: "3",
+    src: "heroes/cheburashka.jpeg",
+  },
+  {
+    width: "3",
+    height: "3",
+    src: "heroes/elon_musk.jpeg",
+  },
+  {
+    width: "3",
+    height: "3",
+    src: "heroes/garfield.png",
+  },
+  {
+    width: "3",
+    height: "3",
+    src: "heroes/harry_potter.jpeg",
+  },
+  {
+    width: "3",
+    height: "3",
+    src: "heroes/jackie_chan.jpeg",
+  },
+  {
+    width: "3",
+    height: "3",
+    src: "heroes/masha.png",
+  },
+  {
+    width: "3",
+    height: "3",
+    src: "heroes/pacman.jpeg",
+  },
+  {
+    width: "3",
+    height: "3",
+    src: "heroes/pikachu.png",
+  },
+  {
+    width: "3",
+    height: "3",
+    src: "heroes/spiderman.jpeg",
+  },
+  {
+    width: "3",
+    height: "3",
+    src: "heroes/spongebob.jpeg",
+  },
+  {
+    width: "3",
+    height: "3",
+    src: "heroes/strdr4605.jpeg",
+  },
+  {
+    width: "3",
+    height: "3",
+    src: "heroes/will_smith.jpeg",
+  },
+  {
+    width: "3",
+    height: "3",
+    src: "heroes/barack_obama.jpeg",
+  },
+  {
+    width: "3",
+    height: "3",
+    src: "heroes/obelix.png",
+  },
+  {
+    width: "3",
+    height: "3",
+    src: "heroes/jerry.png",
+  },
+];
